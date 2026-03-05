f172519559698ed264afd0943daab5842035a05b
denhelloworld <denissaveliev23@gmail.com>
2026-03-05 04:00:37 +0200

    chore(common): add storage service

---
+++ src/modules/comon/helpers/logger.ts
@@ -0,0 +1,7 @@
+import { UniversalLogger } from 'digital-zen-pack/helpers';
+
+/**
+ * Global logger instance
+ * Can be used directly in both Angular and background contexts
+ */
+export const logger = new UniversalLogger();
---
+++ src/modules/comon/services/chrome-storage.service.ts
@@ -0,0 +1,108 @@
+import { ChromeStorageKeyType } from '../enums/chrome-storage-key.enum';
+import { logger } from '../helpers/logger';
+import { Injectable } from '@angular/core';
+
+/**
+ * Chrome Storage service for interacting with chrome.storage.local API
+ * Provides type-safe wrapper around Chrome Extension storage
+ *
+ * @guidelines
+ * - DZ_08: Private fields with # prefix
+ * - DZ_11: Universal Logger usage
+ *
+ * @see /docs/coding-guidelines.md
+ * @see https://developer.chrome.com/docs/extensions/reference/api/storage (Chrome Storage API)
+ */
+@Injectable({
+  providedIn: 'root',
+})
+export class ChromeStorageService {
+  /** @guideline DZ_11 - Universal Logger usage */
+  readonly #logger = logger.createLogger('ChromeStorageService');
+  /**
+   * Writes data to chrome.storage.local.
+   *
+   * @param key The key under which the data will be stored.
+   * @param value The data to be stored.
+   * @param callback Optional function to be called upon completion.
+   */
+  public set<T>(key: ChromeStorageKeyType, value: T, callback?: () => void): void {
+    if (this.#isChromeStorageAvailable()) {
+      chrome.storage.local.set({ [key]: value }, () => {
+        if (chrome.runtime.lastError) {
+          this.#logger.error('Error saving data:', chrome.runtime.lastError);
+        }
+        if (callback) {
+          callback();
+        }
+      });
+    }
+  }
+
+  /**
+   * Reads data from chrome.storage.local.
+   *
+   * @param key The key from which to retrieve the data.
+   * @param callback Function to be called with the retrieved data.
+   */
+  public get<T>(key: ChromeStorageKeyType, callback: (value: T | null) => void): void {
+    if (this.#isChromeStorageAvailable()) {
+      chrome.storage.local.get(key, result => {
+        if (chrome.runtime.lastError) {
+          this.#logger.error('Error reading data:', chrome.runtime.lastError);
+          callback(null);
+        } else {
+          callback((result[key] as T) || null);
+        }
+      });
+    }
+  }
+
+  /**
+   * Reads multiple data entries from chrome.storage.local.
+   *
+   * @param keys The keys from which to retrieve the data.
+   * @param callback Function to be called with the retrieved data.
+   */
+  public getMany<T extends object>(
+    keys: ChromeStorageKeyType[],
+    callback: (value: T | null) => void
+  ): void {
+    if (this.#isChromeStorageAvailable()) {
+      chrome.storage.local.get(keys, result => {
+        if (chrome.runtime.lastError) {
+          this.#logger.error('Error reading multiple keys:', chrome.runtime.lastError);
+          callback(null);
+        } else {
+          callback(result as T);
+        }
+      });
+    }
+  }
+
+  /**
+   * Removes data from chrome.storage.local.
+   *
+   * @param key The key to be removed.
+   * @param callback Optional function to be called upon completion.
+   */
+  public remove(key: ChromeStorageKeyType, callback?: () => void): void {
+    if (this.#isChromeStorageAvailable()) {
+      chrome.storage.local.remove(key as string, () => {
+        if (chrome.runtime.lastError) {
+          this.#logger.error('Error removing data:', chrome.runtime.lastError);
+        }
+        if (callback) {
+          callback();
+        }
+      });
+    }
+  }
+
+  /**
+   * Helper method to check if the chrome storage API is available.
+   */
+  #isChromeStorageAvailable(): boolean {
+    return typeof chrome !== 'undefined' && !!chrome.storage && !!chrome.storage.local;
+  }
+}
